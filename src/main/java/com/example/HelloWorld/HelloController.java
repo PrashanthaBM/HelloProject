b210bef024e753c0aed576a21ad22913899e8b2e
prashi <prashiappu2204@gmail.com>
2025-10-08 21:49:56 +0530

    second commit

---
+++ src/main/java/com/example/HelloWorld/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.HelloWorld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String name()
+    {
+        return "Hello world";
+    }
+
+    @GetMapping("/hello")
+    public String dev2()
+    {
+        return "Hello world - dev 2 added";
+    }
+}
